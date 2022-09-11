757dfcaf03b393292eccaa0cd36e01206f17d2b4
ali <a_icam@yahoo.com>
2022-09-11 16:05:06 -0700

    initial login

---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-card style="padding: 45px" width="600">
+        <v-row justify="center">
+          <h2>Login Form</h2>
+        </v-row>
+        <v-select
+          :items="['student', 'university', 'admin']"
+          label="Select your role"
+        >
+        </v-select>
+        <v-text-field
+          label="username"></v-text-field>
+        <v-text-field
+          label="password" type="password"></v-text-field>
+        <v-card-actions>
+          <v-btn color="#D6CFC2" outlined>Login</v-btn>
+          <v-btn color="#630A0B">Forgot Password</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
